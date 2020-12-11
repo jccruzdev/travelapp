e77ae1d566bbeee5d8fef708479133cf60c02866
ddaniel27 <67126972+ddaniel27@users.noreply.github.com>
2020-12-10 23:19:37 -0500

    Ruta de operador reserva

--- controllers/operador.js
+++ controllers/operador.js
@@ -5,3 +5,9 @@ exports.getIndex = function (req, res) {
 exports.getSuccess = function (req, res) {
   res.render('operador/success');
 };
+
+exports.getReservaOp = async function (req, res){
+  const place = await Place.findOne();
+  const destinoId = place._id;
+  res.render('operador/reservar',{destinoId});
+};
